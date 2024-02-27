8c694e1646cf2a88cafc034133cdc62f08fc2bdf
Simoin Lund <you@example.com>
2024-02-27 09:39:23 +0100

    emmaisin

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 import processing.core.*;
-
 import classes.*;
 
-public class App  extends PApplet{
 
+public class App  extends PApplet{
+    PImage img;
     Hangman game;
     public static void main(String[] args) {
         String[] processingArgs = {"MySketch"};
@@ -17,12 +17,14 @@ public class App  extends PApplet{
 
     public void setup(){
         game = new Hangman("yayyyy");
+        img = loadImage("img.png");
     }
 
     public void draw(){
         background(0);
         game.drawGuessedLetters(this);
         game.drawGallow(this, new PVector(500,100));
+        image(img, 0, 0, 100, 100);
     }
 
     public void keyPressed(){
